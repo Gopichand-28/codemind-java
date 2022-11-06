63ee31485cd601377125d334c28e43e8ebbf0b25
Gopichand-28 <109718382+Gopichand-28@users.noreply.github.com>
2022-11-06 12:51:46 +0530

    import java.util.*; class m{     public static void main(String[] args) {  Scanner s=new Scanner(System.in); int r; r=s.nextInt(); double h=(3.14*(r*r)); System.out.println(h);  } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class m{
+    public static void main(String[] args)
+{
+ Scanner s=new Scanner(System.in);
+int r;
+r=s.nextInt();
+double h=(3.14*(r*r));
+System.out.println(h);
+ }
+}
